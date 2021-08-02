a0a365cc0925016e2134e611944d286da9287cf4
Julio García <julio.garcia@hey.com>
2021-08-02 13:19:53 -0500

    Rename example to preview

--- preview/index.tsx
+++ preview/index.tsx
@@ -1,12 +1,11 @@
-import "react-app-polyfill/ie11";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import Thing from "../.";
+import { DueDate } from "../dist";
 
 const App = () => {
   return (
     <div>
-      <Thing />
+      <DueDate />
     </div>
   );
 };
