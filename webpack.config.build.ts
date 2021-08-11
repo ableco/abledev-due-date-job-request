d9d552c89ec465d5dc0e46bb3535bdfbb97ce47e
Julio García <julio.garcia@hey.com>
2021-08-11 16:20:54 -0500

    Update abledev-components

--- webpack.config.build.ts
+++ webpack.config.build.ts
@@ -3,7 +3,7 @@ import { createWebpackBuildConfigs } from "@ableco/abledev-dev-environment";
 
 export default createWebpackBuildConfigs({
   entries: {
-    web: "./src/index.js",
+    web: "./src/index.tsx",
     node: "./abledev/createHandleRequest.ts",
   },
   distPath: path.resolve(__dirname, "dist"),
