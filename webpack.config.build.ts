d0d11b014f4041e816652eeee3344c156b11227a
Julio García <julio.garcia@hey.com>
2021-08-11 15:52:39 -0500

    Allow JS files

--- webpack.config.build.ts
+++ webpack.config.build.ts
@@ -1,8 +1,10 @@
+import path from "path";
 import { createWebpackBuildConfigs } from "@ableco/abledev-dev-environment";
 
 export default createWebpackBuildConfigs({
   entries: {
-    web: "./src/index.tsx",
+    web: "./src/index.js",
     node: "./abledev/createHandleRequest.ts",
   },
+  distPath: path.resolve(__dirname, "dist"),
 });
--- webpack.config.dev.ts
+++ webpack.config.dev.ts
@@ -3,15 +3,4 @@ import { createWebpackDevConfig } from "@ableco/abledev-dev-environment";
 export default createWebpackDevConfig({
   entry: "./preview/index.tsx",
   template: "./preview/index.html",
-  __internal__extendConfig: {
-    // TODO: Move this config to the main package
-    module: {
-      rules: [
-        {
-          test: /\.css$/i,
-          use: ["style-loader", "css-loader", "postcss-loader"],
-        },
-      ],
-    },
-  },
 });
