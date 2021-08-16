e1f4561638d428ced27e9c636fa8eed4ed310b85
Ricardo Díaz <ricardotk002@gmail.com>
2021-08-16 14:05:07 -0500

    Add steps to auto-merge, auto-migrate and auto-commit the migrations

--- migrations-lib/insert.ts
+++ migrations-lib/insert.ts
@@ -1,4 +1,4 @@
-import shells from "shelljs";
+import shell from "shelljs";
 import path from "path";
 import fs from "fs";
 
@@ -17,35 +17,84 @@ function copyFiles(
     console.log("Copying", file);
 
     if (options.overwrite) {
-      shells.cp("-Rf", file, dest);
+      shell.cp("-Rf", file, dest);
     } else {
-      shells.cp("-Rn", file, dest);
+      shell.cp("-Rn", file, dest);
     }
   });
 }
 
-function moveMigrations() {
-  const directoryPath = path.join(process.cwd(), "prisma/migrations");
+function moveMigrations(
+  libraryCwd: string = process.cwd(),
+  userCwd: string | undefined = process.env.INIT_CWD,
+): string[] {
+  if (!userCwd) return [];
 
+  const directoryPath = path.join(libraryCwd, "prisma/migrations");
   const files = fs.readdirSync(directoryPath);
-
-  console.log("files", files);
-
   const filesToCopy = files
     .filter(
       (file) => !file.endsWith("_setup") && file !== "migration_lock.toml",
     )
     .map((file) => path.join(directoryPath, file));
+  const targetDir = path.join(userCwd, "db/migrations");
+  copyFiles(filesToCopy, targetDir);
 
-  const userPath = process.env.INIT_CWD;
+  return filesToCopy;
+}
 
-  console.log("userPath", userPath);
-  console.log("filesToCopy", filesToCopy);
+function moveSchema(
+  libraryCwd: string = process.cwd(),
+  userCwd: string | undefined = process.env.INIT_CWD,
+) {
+  if (!userCwd) return;
 
-  if (userPath) {
-    const targetDir = path.join(userPath, "db/migrations");
-    copyFiles(filesToCopy, targetDir);
+  const source = path.join(libraryCwd, "prisma/schema.prisma");
+  const target = path.join(userCwd, "db/prisma-new.prisma");
+  copyFiles([source], target);
+}
+
+function mergeSchema(userCwd: string | undefined = process.env.INIT_CWD) {
+  if (!userCwd) return;
+
+  const base = path.join(userCwd, "db/schema.prisma");
+  const changed = path.join(userCwd, "db/prisma-new.prisma");
+
+  if (shell.exec(`git merge-file ${base} ${base} ${changed}`).code !== 0) {
+    shell.echo("Error: Auto-merge failed");
   }
 }
 
-moveMigrations();
+function autoApplyMigrations(
+  userCwd: string | undefined = process.env.INIT_CWD,
+) {
+  if (!userCwd) return;
+
+  if (shell.exec("npx blitz prisma migrate dev", { cwd: userCwd }).code !== 0) {
+    shell.echo("Error: Applying migrations failed");
+  }
+}
+
+function autoCommitChanges(
+  migrationName: string,
+  userCwd: string | undefined = process.env.INIT_CWD,
+) {
+  if (!userCwd) return;
+
+  if (
+    shell.exec(
+      `git add . && git commit -am "New migrations applied: ${migrationName}"`,
+      {
+        cwd: userCwd,
+      },
+    ).code !== 0
+  ) {
+    shell.echo("Error: Applying migrations failed");
+  }
+}
+
+const migrations = moveMigrations();
+moveSchema();
+mergeSchema();
+autoApplyMigrations();
+// autoCommitChanges(migrations.join(", "));
