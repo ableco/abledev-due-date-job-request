3e776aa72cc1f224698e2a5a0d8d4e557a4d6819
Julio García <julio.garcia@hey.com>
2021-08-26 23:43:25 -0500

    Improve getPreviewData function

--- preview/getPreviewData.ts
+++ preview/getPreviewData.ts
@@ -2,8 +2,49 @@ import { AsyncReturnType } from "type-fest";
 import { HostContextType } from "../src/HostContext";
 
 async function getPreviewData({ db }: HostContextType) {
-  const allTasks = await db.task.findMany({});
-  return { allTasks };
+  const currentUser = await ensureCurrentUser(db);
+
+  if ((await db.task.count()) <= 1) {
+    await db.task.createMany({
+      data: [
+        { description: "New Task 1", creatorId: currentUser.id },
+        { description: "New Task 2", creatorId: currentUser.id },
+        { description: "New Task 3", creatorId: currentUser.id },
+      ],
+    });
+  }
+
+  return {
+    allTasks: await db.task.findMany({
+      where: {
+        OR: [{ assignedUserId: currentUser.id }, { creatorId: currentUser.id }],
+      },
+    }),
+  };
+}
+
+async function ensureCurrentUser(db: HostContextType["db"]) {
+  const email = "email@example.com";
+  const domain = "example.com";
+
+  return (
+    (await db.user.findFirst({
+      where: {
+        email,
+      },
+    })) ??
+    (await db.user.create({
+      data: {
+        email,
+        organization: {
+          connectOrCreate: {
+            where: { domain },
+            create: { domain },
+          },
+        },
+      },
+    }))
+  );
 }
 
 export type PreviewData = AsyncReturnType<typeof getPreviewData>;
